53cbe2b5945dc10299f10cbb0830e628817897bc
charlieowen3065 <charles.owen@austin.utexas.edu>
2024-10-30 12:35:20 -0500

    Replaced 'solution' and 'residual' outputs with general 'vector' output

--- src/outputs/LinearSystemVectorOutput.C
+++ src/outputs/LinearSystemVectorOutput.C
@@ -1,34 +1,51 @@
-#include "SolutionVectorOutput.h"
+#include "LinearSystemVectorOutput.h"
 
-registerMooseObjectAliased("BubbleFlowApp", SolutionVectorOutput, "SolutionVectorOutput");
+registerMooseObjectAliased("BubbleFlowApp", LinearSystemVectorOutput, "LinearSystemVectorOutput");
 
 InputParameters
-SolutionVectorOutput::validParams()
+LinearSystemVectorOutput::validParams()
 {
     InputParameters params = LinearSystemBaseOutput::validParams();
+
+    params.addParam<std::string>("vector_type", "Type of vector to output {solution, residual}");
 
     return params;
 }
 
-SolutionVectorOutput::SolutionVectorOutput(const InputParameters & parameters)
-  :  LinearSystemBaseOutput(parameters)
-{}
-
-void
-SolutionVectorOutput::output()  
+LinearSystemVectorOutput::LinearSystemVectorOutput(const InputParameters & parameters)
+  : LinearSystemBaseOutput(parameters),
+    _system_vector(static_cast<ImplicitSystem &>(_nl.system()).get_vector(0)),
+    _vector_type(getParam<std::string>("vector_type"))
 {
-    variableSolutions();
+    loadVector();
 }
 
 void
-SolutionVectorOutput::outputSolutionOfSingleVariable(std::string variable_name)
+LinearSystemVectorOutput::output()  
+{
+    variableVectors();
+}
+
+void
+LinearSystemVectorOutput::loadVector()
+{
+    if (_vector_type == "solution")
+        _system_vector = _nl.solution();
+    else if (_vector_type == "residual")
+        _system_vector = _nl.RHS();
+    else
+        mooseError("ERROR: " + _vector_type + " is invalid.");
+}
+
+void
+LinearSystemVectorOutput::outputVectorOfSingleVariable(std::string variable_name)
 {
     int variable_number = _nl.getVariable(_tid, variable_name).number();
     unsigned int number_componets = _mesh.nodePtr(0)->n_comp(_sys_number, variable_number);
     std::vector<dof_id_type> dof_indices;
     dof_id_type** component_array = getDOFIndices_SingleVariable(dof_indices, _sys_number,
                                                                  variable_number, number_componets);
-    Real** solutionArray = createSolutionOutputArrayOfSingleVariable(component_array, number_componets);
+    Real** solutionArray = createVectorOutputArrayOfSingleVariable(component_array, number_componets);
     
     // Output
     std::vector<std::string> column_names;
@@ -38,15 +55,14 @@ SolutionVectorOutput::outputSolutionOfSingleVariable(std::string variable_name)
         std::vector<std::string> vec_components = {"x", "y", "z"};
         for (auto i : make_range(number_componets)) {column_names.push_back("u_" + vec_components[i]);}
     }
-    std::string base_filename = getFilename(variable_name + "_solution");
+    std::string base_filename = getFilename(variable_name + "_" + _vector_type);
     outputToCSV(solutionArray, column_names, 1 + number_componets, _num_nodes, base_filename);
 }
 
 Real**
-SolutionVectorOutput::createSolutionOutputArrayOfSingleVariable(dof_id_type** component_array, int number_components)
+LinearSystemVectorOutput::createVectorOutputArrayOfSingleVariable(dof_id_type** component_array, int number_components)
 {
     Real** OutputArray = new Real*[_num_nodes];
-    const NumericVector<Real> * current_solution = _nl.currentSolution();
     
     for (auto node_idx : make_range(_num_nodes))
     {
@@ -55,14 +71,14 @@ SolutionVectorOutput::createSolutionOutputArrayOfSingleVariable(dof_id_type** co
         for (auto ci : make_range(number_components))
         {
             dof_id_type dof_i = component_array[node_idx][4 + ci];
-            OutputArray[node_idx][1+ci] = current_solution->el(dof_i);
+            OutputArray[node_idx][1+ci] = _system_vector.el(dof_i);
         }
     }
     return OutputArray;
 }
 
 void
-SolutionVectorOutput::variableSolutions()
+LinearSystemVectorOutput::variableVectors()
 {
     std::vector<VariableName> var_names;
     if ((_output_variable_name != "all") && (_output_variable_name != "none"))
@@ -75,10 +91,10 @@ SolutionVectorOutput::variableSolutions()
     }
 
     for (auto var_name : var_names){
-        outputSolutionOfSingleVariable(var_name);
+        outputVectorOfSingleVariable(var_name);
     }
 }
 
 void
-SolutionVectorOutput::fullSolution()
+LinearSystemVectorOutput::fullVector()
 {}
